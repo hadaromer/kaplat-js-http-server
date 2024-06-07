d7c4c5ba6d364fcaaf6b7abf1e2c714813f29d1e
Omer Hadar <omer.hadar14@gmail.com>
2024-06-07 17:32:29 +0300

    add class validator

--- src/book/book.dto.ts
+++ src/book/book.dto.ts
@@ -1,7 +1,14 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class BookDto {
+  @IsNotEmpty()
   readonly title: string;
+  @IsNotEmpty()
   readonly author: string;
+  @IsNotEmpty()
   readonly year: number;
+  @IsNotEmpty()
   readonly price: number;
+  @IsNotEmpty()
   readonly genres: string[];
 }
